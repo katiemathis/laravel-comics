8c75d08beeb8f135819e62dcf3013ea0b3db66af
katiepalumbo <palumbo.katie@gmail.com>
2022-03-28 22:03:01 +0200

    aggiustamenti css

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -3,26 +3,28 @@
 @section('pageTitle', 'Home')
 
 @section('content')
-    <div class="container">
-        <button id="top"><a href="#">CURRENT SERIES</a></button>
-        <div class="product-cards-container">
-            <div class="product-cards">
-                @foreach($items as $item)
-                    <div class="product-card">
-                        <div class="product-image">
-                            <img src="{{$item['thumb']}}" alt="product thumbnail">
-                        </div>
-                        <div class="product-series">
-                            <h1>{{$item['series']}}</h1>
-                        </div>
-                    </div>
-
-                @endforeach
-
-
-            </div>
+    <div class="body-container" >
+        <div class="jumbotron-img">
+            <img src="{{asset('img/jumbotron.jpg')}}" alt="">
         </div>
-        <button id="bottom"><a href="#">LOAD MORE</a></button>
+        <div class="container">
+            <button id="top"><a href="#">CURRENT SERIES</a></button>
+            <div class="product-cards-container">
+                <div class="product-cards">
+                    @foreach($items as $item)
+                        <div class="product-card">
+                            <div class="product-image">
+                                <img src="{{$item['thumb']}}" alt="product thumbnail">
+                            </div>
+                            <div class="product-series">
+                                <h1>{{$item['series']}}</h1>
+                            </div>
+                        </div>
+                    @endforeach
+                </div>
+            </div>
+            <button id="bottom"><a href="#">LOAD MORE</a></button>
 
+        </div>
     </div>
 @endsection('content')
